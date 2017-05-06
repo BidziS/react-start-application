51a178a186f47f1ca83ccf7008ee08bc3a7e2851
BidziS <danielcudnik93@gmail.com>
2017-05-06 12:36:40 +0200

    int

---
+++ app/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export class App extends Component {
+    state = {
+        selectedNumbers: [2,4]
+    }
+    render() {
+        return (
+        <div className="container">
+            Siemano
+        </div>
+        );
+    }
+}
